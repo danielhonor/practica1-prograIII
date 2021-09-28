64523cc4f47d85dc3da380cc094e1f0518b66121
danielhonor <danitocabello18@gmail.com>
2021-09-28 10:10:08 -0400

    cupertinoTabScaffold clase8 28/09/21

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto1/places.dart';
+import 'package:proyecto1/places_cupertino.dart';
 
 
 import 'home.dart';
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget{
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity
       ),
-      home: Places(),
+      home: PlacesCupertino(),
     );
   }
 
